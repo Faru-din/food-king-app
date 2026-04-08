6b2773cec8e07b56ad482055ed60cb75296dda67
Faru-din <135270624+Faru-din@users.noreply.github.com>
2026-04-08 13:19:19 +0000

    Fix main.dart build method

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
 
 import 'core/theme.dart';
 import 'providers/auth_provider.dart';
 import 'providers/cart_provider.dart';
 import 'providers/menu_provider.dart';
-import 'screens/auth/login_screen.dart';
-import 'screens/home/home_screen.dart';
 import 'screens/splash/splash_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // NOTE: Firebase initialization requires `flutterfire configure` to be run first.
-  // Uncomment the lines below after Firebase is set up.
-  /*
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  */
 
   runApp(
     MultiProvider(
@@ -29,19 +17,21 @@ void main() async {
         ChangeNotifierProvider(create: (_) => MenuProvider()),
         ChangeNotifierProvider(create: (_) => CartProvider()),
       ],
-      child: const FoodKingApp(),
+      child: FoodKingApp(),
     ),
   );
 }
 
 class FoodKingApp extends StatelessWidget {
-   FoodKingApp({super.key});
+  FoodKingApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Food-King',
       theme: AppTheme.lightTheme,
       debugShowCheckedModeBanner: false,
-      home:  SplashScreen(),
+      home: SplashScreen(),
     );
   }
 }
